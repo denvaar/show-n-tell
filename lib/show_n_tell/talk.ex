b3ecb4516eafd375cef45d7215deef6cd1bdb3bc
Andrew Price <ayjay1212@hotmail.com>
2018-04-03 21:26:15 -0600

    Talks schema migration

---
+++ lib/show_n_tell/talk.ex
@@ -0,0 +1,21 @@
+defmodule ShowNTell.Talk do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+
+  schema "talks" do
+    field :date, :date
+    field :description, :string
+    field :estimated_duration, :integer
+    field :title, :string
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(talk, attrs) do
+    talk
+    |> cast(attrs, [:title, :description, :estimated_duration, :date])
+    |> validate_required([:title, :description, :estimated_duration, :date])
+  end
+end
