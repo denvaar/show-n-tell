6752acbcc96219878fbc39201b503369963b6ee8
Denver Smith <denverpsmith@gmail.com>
2018-04-11 14:56:14 -0600

    [#4] Add user schema and migration

---
+++ priv/repo/migrations/20180411205345_create_users.exs
@@ -0,0 +1,15 @@
+defmodule ShowNTell.Repo.Migrations.CreateUsers do
+  use Ecto.Migration
+
+  def change do
+    create table(:users) do
+      add :first_name, :string
+      add :last_name, :string
+      add :email, :string
+      add :github_token, :string
+
+      timestamps()
+    end
+
+  end
+end
