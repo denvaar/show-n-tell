bad11456881bb3767042ac0503588bae818c0187
Alex Kovshovik <alex@shovik.com>
2018-06-17 15:24:07 -0600

    #8: Add seeds for users and talks

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,22 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias ShowNTell.Repo
+alias ShowNTell.Talk
+alias ShowNTell.User
+
+%Talk{date: ~D[2018-07-01], title: "How to like your own code", description: "Best practices of writing code for humans", estimated_duration: 20}
+|> Repo.insert!
+
+%Talk{date: ~D[2018-07-01], title: "On yelling at your co-workers", description: "About some colorful language to be used while yelling", estimated_duration: 15}
+|> Repo.insert!
+
+%Talk{date: ~D[2018-07-08], title: "Work/Life balance", description: "No such thing", estimated_duration: 60}
+|> Repo.insert!
+
+%User{email: "mr.bator@gmail.com", first_name: "Andrew", last_name: "Bator", github_token: "123456789"}
+|> Repo.insert!
+
+%User{email: "uber.coder@gmail.com", first_name: "Denver", last_name: "Smith", github_token: "987654321"}
+|> Repo.insert!
