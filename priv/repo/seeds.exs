ac17ffdfb4817ab02a8337d05dc308e175ad3354
Alex Kovshovik <alex@shovik.com>
2018-06-18 08:32:02 -0600

    #8: Set proper association between talks and users in seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -11,20 +11,19 @@
 # and so on) as they will fail if something goes wrong.
 
 alias ShowNTell.Repo
-alias ShowNTell.Talk
 alias ShowNTell.User
 
-%Talk{date: ~D[2018-07-01], title: "How to like your own code", description: "Best practices of writing code for humans", estimated_duration: 20}
+andrew = %User{email: "mr.bator@gmail.com", first_name: "Andrew", last_name: "Bator", github_token: "123456789"}
+         |> Repo.insert!
+
+denver = %User{email: "uber.coder@gmail.com", first_name: "Denver", last_name: "Smith", github_token: "987654321"}
+         |> Repo.insert!
+
+Ecto.build_assoc(andrew, :talks, %{date: ~D[2018-07-01], title: "How to like your own code", description: "Best practices of writing code for humans", estimated_duration: 20})
 |> Repo.insert!
 
-%Talk{date: ~D[2018-07-01], title: "On yelling at your co-workers", description: "About some colorful language to be used while yelling", estimated_duration: 15}
+Ecto.build_assoc(andrew, :talks, %{date: ~D[2018-07-01], title: "On yelling at your co-workers", description: "About some colorful language to be used while yelling", estimated_duration: 15})
 |> Repo.insert!
 
-%Talk{date: ~D[2018-07-08], title: "Work/Life balance", description: "No such thing", estimated_duration: 60}
-|> Repo.insert!
-
-%User{email: "mr.bator@gmail.com", first_name: "Andrew", last_name: "Bator", github_token: "123456789"}
-|> Repo.insert!
-
-%User{email: "uber.coder@gmail.com", first_name: "Denver", last_name: "Smith", github_token: "987654321"}
+Ecto.build_assoc(denver, :talks, %{date: ~D[2018-07-08], title: "Work/Life balance", description: "No such thing", estimated_duration: 60})
 |> Repo.insert!
